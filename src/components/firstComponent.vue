d0620a1e36ef32981f372c3efdc096c963fceaf2
jestfer <jestfer6@gmail.com>
2020-06-14 14:13:10 +0100

    Added new component and route to it

---
+++ src/components/firstComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="firstComponent">
+    <h1>First component template</h1>
+
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: 'This is my first component in View'
+    }
+  }
+}
+</script>
+
+<style></style>
